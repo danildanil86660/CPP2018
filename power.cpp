f2b23f779cfa02ed649d495a6c88402bb1a553c4
Danila Petrov <dan0712@yandex.ru>
2018-07-11 11:26:56 +0300

    update power.cpp# Please enter the commit message for your changes. Lines starting

--- power.cpp
+++ power.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 int main()
 {
-int b=2;
-for(int i=0;i<10;++i)
-{
-int p;
-p=1;
-for(int t=1;t<=i;++t)
-p=p*b;
-std::cout<<p<<" ";
-}
-std::cout<<std::endl;
-return 0;
+    int nPowerBase=2;
+    for(int i=0;i<10;++i)
+    {
+        int nBase;
+        nBase=1;
+        for(int t=1;t<=i;++t)
+        nBase=nBase*nPowerBase;
+        std::cout<<p<<" ";
+    }
+    std::cout<<std::endl;
+    return 0;
 }
